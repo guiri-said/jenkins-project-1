d71888a10b8b97dd8c10579671a0524cd1e584c6
guiri-said <56483759+guiri-said@users.noreply.github.com>
2024-12-15 09:05:12 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("You should see this sentence in the Jenkins job console !");
+        System.out.println("You should see alos this sentence !");
     }
 }
